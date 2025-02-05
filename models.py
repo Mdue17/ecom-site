4b9526aa1b50e141b45b63bcf4465df9cdd02095
ndube023 <ndube023@student.wethinkcode.co.za>
2025-02-05 09:05:31 +0200

    models for users in the db

--- models.py
+++ models.py
@@ -1,8 +1,23 @@
 from datetime import datetime
 
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    """Model for Users in db"""
+    id = db.Column(db.Interger, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def chech_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Category(db.Model):
     """Model for the categories table"""
